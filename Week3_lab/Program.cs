94b1cc4bc30853d931fd348adbaafa0c21140063
TheJade <58971378+TheJadex@users.noreply.github.com>
2022-04-23 22:14:42 +0100

    Add files via upload

---
+++ Week3_lab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+ 
+namespace Week3_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number to check: ");
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+ 
+            int divisor  = 0;
+ 
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    divisor ++;
+                }
+            }
+            if (divisor == 2)
+            {
+                Console.WriteLine("The entered number is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine("The entered number is not a prime number.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Week4-lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Week4_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, result;  
+   
+            Console.Write("Enter first number: ");  
+            num1 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Enter second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            result = num1 + num2;
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, result);
+            if(result < 100){
+                Console.WriteLine("Your answer is less than 100. Therefore, your result is " + (result < 100));
+            }
+            else{
+                Console.WriteLine("Your answer is greater than 100. Therefore, your result is " + (result < 100));
+            }
+            
+        }
+    }
+}
